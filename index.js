641410938816685aa0ab3d985636577e39b4974b
axdi611 <56131854+axdi611@users.noreply.github.com>
2024-04-03 15:11:57 +0530

    index.js

--- index.js
+++ index.js
@@ -1,71 +1,17 @@
-let trends = document.querySelectorAll(".trends");
-let button = document.querySelectorAll(".cart");
-let  cards = document.querySelectorAll(".card");
+let sections  = document.querySelectorAll('section');
 
-let count = 0;
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
 
-trends.forEach((curElem, index)=>{
-    curElem.style.left=`${index*100}%`;
-})
-
-
-const myFood  = ( ) =>{
-    trends.forEach((curImg)=>{
-        curImg.style.transform=`translateX(-${count * 100}%)`
-    })
-     
-}
-
-setInterval(()=>{
-    count++;
-    if(count == trends.length){
-        count=0
-    }
-    myFood()
-},4000)
-
-// cart
-button.forEach((curBtn)=>{
-    curBtn.addEventListener("click", function(){
-        alert("Added To Cart")
-    })
-})
-
-
-// card detail
-cards.forEach((curCard)=>{
-    curCard.addEventListener("click", function(){
-        console.log(curCard);
-
-        let div = document.createElement("div");
-        div.classList.add("cardDetail");
-        div.innerHTML=`
-        <i id="icon" class="fa-solid fa-xmark"></i>
-        <img src=${curCard.firstElementChild.src} alt="">
-        <h1>Fresh Foods</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        Recusandae odio velit mollitia nulla inventore asperiores fuga. Quasi, eius
-         nemo. Blanditiis, consequatur sunt.</p>
-
-        `
-        document.querySelector("body").appendChild(div)
-        document.getElementById("icon").addEventListener("click", function(){
-            div.remove();
-        })
-    })
-})
-document.addEventListener('DOMContentLoaded', function() {
-    const dropdownToggle = document.querySelector('.logo');
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-
-    dropdownToggle.addEventListener('click', function() {
-        dropdownMenu.classList.toggle('show');
-    });
-
-    // Close the dropdown menu when clicking outside of it
-    window.addEventListener('click', function(event) {
-        if (!dropdownToggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
-            dropdownMenu.classList.remove('show');
+        if (top >= offset && top < offset + height){
+            sec.classList.add('show-animate');
         }
-    });
-});
+        else{
+            sec.classList.remove('show-animate');
+        }
+    })
+        
+}
